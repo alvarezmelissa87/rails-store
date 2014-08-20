3254b3368d4cb678c23b8aeb92ec1634922b7f76
Melissa Alvarez <alvarezmelissa87@gmail.com>
2014-08-19 19:45:04 -0700

    Added some fixtures and some model tests

--- depot/test/models/product_test.rb
+++ depot/test/models/product_test.rb
@@ -27,4 +27,34 @@ class ProductTest < ActiveSupport::TestCase
     product.price = 1
     assert product.valid?
   end
+
+  def new_product(image_url)
+    Product.new(title:       "My Book Title",
+                description: "yyy",
+                price:       1,
+                image_url:   image_url)
+  end
+
+  test "image url" do
+    ok = %w{ fred.gif fred.jpg fred.png FRED.JPG FRED.jpg
+             http://a.b.c/x/y/z/fred.gif }
+    bad = %w{ fred.doc fred.gif/more fred.gif.more }
+
+    ok.each do |name|
+      assert new_product(name).valid?, "#{name} shouldn't be valid"
+    end
+
+    bad.each do |name|
+      assert new_product(name).invalid?, "#{name} shouldn't be valid"
+    end
+
+    test "product is not valid without a unique title" do
+      product = Product.new(title:        products(:ruby).title,
+                            description:  "yyy",
+                            price:        1,
+                            image_url:    "fred.gif")
+
+      assert product.invalid?
+      assert_equal ["has already been taken"], product.errors[:title]
+    end
 end
