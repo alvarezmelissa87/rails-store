24aaf2f92a30f70cd896524454f27873c62c1105
Melissa Alvarez <alvarezmelissa87@gmail.com>
2014-08-29 13:56:18 -0700

    Added validation to order model to make sure all input fields get filled and payment method is one of accepted values

--- depot/app/models/order.rb
+++ depot/app/models/order.rb
@@ -1,3 +1,5 @@
 class Order < ActiveRecord::Base
+  validates :name, :address, :email, presence: true
+  validates :pay_type, inclusion: PAYMENT_TYPES
   PAYMENT_TYPES = ["Check", "Credit card", "Purchase order"]
 end
