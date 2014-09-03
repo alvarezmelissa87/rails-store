abef78d804b265d4181c16a2db0cd2d36cffcfd7
Melissa Alvarez <alvarezmelissa87@gmail.com>
2014-09-03 11:07:35 -0700

    Added bcrypt gem and adjusted user controller to redirect to user index instead of user show after creation since password won't be shown

--- depot/app/models/user.rb
+++ depot/app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ActiveRecord::Base
+  validates :name, presence: true, uniqueness: true
   has_secure_password
+  #this tells rails to validate that the two passwords match when user enters password then reenters it
 end
